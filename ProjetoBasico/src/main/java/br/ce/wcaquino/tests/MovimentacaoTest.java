735cfdda0eb63fe4b9f3adb8216a39ff7a4120fd
bruno-maker <brunomakoto7@gmail.com>
2020-04-01 22:02:46 -0300

    selenium

---
+++ ProjetoBasico/src/main/java/br/ce/wcaquino/tests/MovimentacaoTest.java
@@ -0,0 +1,52 @@
+package br.ce.wcaquino.tests;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.ce.wcaquino.core.BaseTest;
+import br.ce.wcaquino.pages.MenuPage;
+import br.ce.wcaquino.pages.MovimentacaoPage;
+
+
+public class MovimentacaoTest extends BaseTest{
+	
+	private MenuPage menuPage = new MenuPage();
+	private MovimentacaoPage movPage = new MovimentacaoPage();
+	
+	@Test
+	public void testInserirMovimentacao(){
+		menuPage.acessarTelaInserirMovimentacao();
+		
+		movPage.setDataMovimentacao("01/09/2018");
+		movPage.setDataPagamento("02/09/2018");
+		movPage.setDescricao("Movimentação do Teste");
+		movPage.setDescricao("Interessado Qualquer");
+		movPage.setValor("500");
+		movPage.setConta("outra conta 2");
+		movPage.setStatusPago();
+		movPage.salvar();
+		Assert.assertEquals("Movimentação adicionada com sucesso!" , movPage.obterMensagemSucesso());
+	}
+	
+	@Test
+	public void testCamposObrigatorios() {
+		menuPage.acessarTelaInserirMovimentacao();
+		
+		movPage.salvar();
+		List<String> erros = movPage.obterErros();
+//		Assert.assertEquals("Data da Movimentação é obrigatório", erros.get(0));
+//		Assert.assertTrue(erros.contains("Data da Movimentação é obrigatório"));
+		Assert.assertTrue(erros.containsAll(Arrays.asList(
+				"Data da Movimentação é obrigatório",
+				"Data do pagamento é obrigatório",
+				"Descrição é obrigatório",
+				"Iteressado é obrigatório",
+				"Valor é obrigatório",
+				"Valor deve ser um número"
+				)));
+		Assert.assertEquals(6, erros.size());
+	}
+}
